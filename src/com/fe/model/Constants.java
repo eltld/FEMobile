a905b5db1016be401c03a9762749d3b8cb4665c7
manuonda <manuonda@gmail.com>
2014-07-28 03:31:20 +0200

    Constants.java

---
+++ src/com/fe/model/Constants.java
@@ -0,0 +1,12 @@
+package com.fe.model;
+
+public final class Constants {
+
+	private static final String DATABASE_NAME="fe";
+	private static final String APP_NAME="APPFE";
+	private static final String FILE_NAME="db_fe";
+	private static final String TABLE_NEWS="TB_NEWS";
+	
+	
+	
+}
