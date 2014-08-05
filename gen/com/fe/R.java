640ba0143135d6e989713e34d7e9fc5b93fca61f
manuonda <manuonda@gmail.com>
2014-08-05 15:27:14 -0300

    modificate file

--- gen/com/fe/R.java
+++ gen/com/fe/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int grid_principal=0x7f070000;
-        public static final int imageItem=0x7f070001;
-        public static final int image_header=0x7f070004;
-        public static final int image_news=0x7f070003;
-        public static final int list_secretaria=0x7f070005;
-        public static final int textItem=0x7f070002;
-        public static final int text_secretaria=0x7f070006;
+        public static final int grid_principal=0x7f070002;
+        public static final int imageItem=0x7f070003;
+        public static final int image_header=0x7f070006;
+        public static final int image_news=0x7f070005;
+        public static final int list_secretaria=0x7f070007;
+        public static final int scro1=0x7f070001;
+        public static final int textItem=0x7f070004;
+        public static final int text_secretaria=0x7f070008;
+        public static final int txtTextView=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
