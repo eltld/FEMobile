359a084d1714568aaedccac2c294804d1ee2f884
manuonda <manuonda@gmail.com>
2014-08-11 04:02:45 +0200

    files agregate

---
+++ src/com/fe/service/ServiceUrl.java
@@ -0,0 +1,8 @@
+package com.fe.service;
+
+public abstract class ServiceUrl {
+	
+	private static final String SERVICE_URL="http://localhost/";
+	private static final String SERVICE_NEWS="http://";
+
+}
