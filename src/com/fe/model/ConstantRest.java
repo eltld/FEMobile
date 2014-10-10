75b15fc9ec0992a55e0ec7371232d5e1ee00cdee
manuonda <manuonda@gmail.com>
2014-10-09 21:46:14 -0300

    modificacion archivos

--- src/com/fe/model/ConstantRest.java
+++ src/com/fe/model/ConstantRest.java
@@ -15,8 +15,8 @@ public abstract  class ConstantRest {
 	//public final static String URL_NOTICIAS="http://10.2.2.245/projects/www/noticias.php";
     //public final static String URL_IMAGES="http://10.2.2.245/WebRest//fotos.php";
     //ip 170.210.240.251 - 10.2.0.66
-	public final static String URL_BASE="http://10.2.0.66/php-manu/";
-	public final static String URL_BASE_NOTICIAS="http://10.2.0.66/noticias/imgnotis/";
+	public final static String URL_BASE="http://170.210.240.251/php-manu/";
+	public final static String URL_BASE_NOTICIAS="http://170.210.240.251/noticias/imgnotis/";
 	
 	public final static String URL_NOTICIAS=URL_BASE+"noticias.php";
     public final static String URL_AUTORIDADES=URL_BASE+"autoridad.php";
